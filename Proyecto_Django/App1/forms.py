c11a6ff429b57c6e68d9ed2122026eaddbc223a3
Davidpl95 <dadavidl34@gmial.com>
2025-04-21 13:47:22 -0500

    CRM_Estudiantes

--- Proyecto_Django/App1/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Producto  # Asegúrate de que `Producto` está en `models.py`
-
-class ProductoForm(forms.ModelForm):
-    class Meta:
-        model = Producto
-        fields = ['nombre', 'precio', 'descripcion', 'stock']
